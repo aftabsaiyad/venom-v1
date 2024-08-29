0e3ac661708661a3a8bebeb3e4fc5a1bb4ad7f20
aftabsaiyad <89257519+aftabsaiyad@users.noreply.github.com>
2024-08-29 10:31:06 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7543594921:AAEb0Qs9usGggKGl0Q-iZ-JimDzJhtv01e0')
+bot = telebot.TeleBot('7542126967:AAE_5RHYMDAHK7IJlvzD1W3p0bJ3XJRBl0I')
 
 # Admin user IDs
-admin_id = ["5894556386"]
+admin_id = ["1667632119"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
@@ -48,10 +48,10 @@ allowed_user_ids = read_users()
 
 # Function to log command to the file
 def log_command(user_id, target, port, time):
-    admin_id = ["5588464519"]
+    admin_id = ["1667632119"]
     user_info = bot.get_chat(user_id)
-    if user_info.username:
-        username = "@" + user_info.username
+    if user_info.Gamner_bbgi:
+        username = "@" + user_infoGamnerbbgi_bot
     else:
         username = f"UserID: {user_id}"
     
